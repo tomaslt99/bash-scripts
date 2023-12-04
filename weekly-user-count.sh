40bf6395ef4e7bb05431cd881459b427c8a474c6
tomaslt99 <53668968+tomaslt99@users.noreply.github.com>
2023-12-04 18:37:52 +0200

    Update weekly-user-count.sh

--- weekly-user-count.sh
+++ weekly-user-count.sh
@@ -37,8 +37,8 @@ systemctl reload crond
 ## Instead of root user, use dedicated sudo user that does not require password for each task, command.
 crontab -e -u root
 
-# Scheduling to run: each 7 minute, between 6-20 hours, from Monday till Friday
-7 6-20 * * 1-5 /usr/local/bin/user-count/weekly-user-count.sh
+# Scheduling to run: every 7,37 minute, between 6-20 hours, from Monday till Friday
+7,37 6-20 * * 1-5 /usr/local/bin/user-count/weekly-user-count.sh
 
 # End of Crontab -----------------------------
 '
@@ -103,27 +103,38 @@ awk '{gsub(/-/, " "); print}' <  weekly.txt >  weekly1.txt
 awk '{gsub("weekly/", ""); print}' <  weekly1.txt >  weekly2.txt
 
 
+
+
 # send email & remove files - START -----------------------------
 
+# If needed, change the date temporarily on a system.
+# sudo timedatectl set-ntp off
+# sudo date -s "8 DEC 2023 20:00:00"
+# if you reboot system will chnage to correct date.
+
+
+#----start -1
 # daily 20:00 delete files: daily-user-log-in.txt & daily-user-count.txt,
 # if files are not deleted daily, next day files will be appended (data incorrect, corrupt)
-#if [ "$(date +%H)" = "20" ]; then
-# For testing purpose use following line
-if [ "$(date +%d)" = "$(date +%d)" ]; then
-# For production use follwoing line
-#if [ "$(date +%H)" = "20" ]; then
+if [ "$(date +%H)" = "20" ]; then
     rm /usr/local/bin/user-count/daily-user*.txt
 fi
+#----end -1
 
-# For testing purpose use following line:
-if [ "$(date +%d)" = "$(date +%d)" ]; then
-echo -e "hostname - $(hostname): \n$(cat /usr/local/bin/user-count/weekly2.txt)"
-
-# Production
+#----start -2
+# In the above script, $(date +%u) returns the current day of the week (1-7, Monday-Sunday), 
+# and $(date +%H) returns the current hour in 24-hour format.
+# The if condition checks if the current day is Friday (-eq 5) and the current hour is 8 PM (-eq 20). 
+# If the condition is true, the script runs the commands inside the if block.
+#if [[ $(date +%u) -eq 1 && $(date +%H) -eq 20 ]]; then
+#    # Run your script here
+#        echo "test-Completed"
+#fi
 # send an email if it's Friday at 20:00
 if [[ $(date +%A) -eq Friday && $(date +%H) -eq 20 ]]; then
-        echo "test-Completed"
-echo "test-Completed" > /usr/local/bin/user-count/test.txt
+
+#echo "test-Completed" > /usr/local/bin/user-count/test.txt
+#----end -2
 
 # Send email - START -----------------------------
 # echo "This is the body of the email" | mail -s "Subject line" -a /path/to/file.txt  recipient@example.com
